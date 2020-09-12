915b78acd1d149c5438a60b8b56b77e1d82ee7de
kotatsugame <takaryou1019@gmail.com>
2020-09-13 06:32:55 +0900

    add test2.pl

--- Convolution/README.md
+++ Convolution/README.md
@@ -22,5 +22,11 @@ MODの値を変更したい場合は、この関数を書き換えてくださ�
 
 [AtCoder Library Practice Contest F - Convolution](https://atcoder.jp/contests/practice2/submissions/16631265)
 
-`test.pl`です。全然間に合いませんでした。
+`test1.pl`です。全然間に合いませんでした。
+
+[AtCoder Typical Contest 001 C - 高速フーリエ変換](https://atcoder.jp/contests/atc001/submissions/16656446)
+
+`test2.pl`です。こちらは余裕をもって間に合いました。
+
+畳み込み後の要素の最大値は`10^5*100^2 = 10^9`程度と見積もることができます。そこで、MODとして`10^9`を超える`1012924417 = 2^21*483+1`を使用することで、値の復元が必要なくなります。このMODに対応する最小の原始根は5なので、その値も書き換えています。
 
---
+++ Convolution/test2.pl
@@ -0,0 +1,107 @@
+use strict;
+use warnings;
+package Convolution;
+use integer;
+sub MOD(){1012924417};
+sub primitive_root(){5};
+sub powermod($$){
+	my($v,$w)=@_;
+	my$r=1;
+	$v%=MOD;
+	while($w>0){
+		$r=$r*$v%MOD if$w&1;
+		$v=$v*$v%MOD;
+		$w>>=1;
+	}
+	return$r;
+}
+
+my(@es,@ies,@sum_e,@sum_ie);
+{
+	my$cnt2=0;
+	$cnt2++until MOD-1>>$cnt2&1;
+	my$e=powermod(primitive_root,MOD-1>>$cnt2);
+	my$ie=powermod($e,MOD-2);
+	for(my$i=$cnt2;$i>=2;$i--){
+		$es[$i-2]=$e;
+		$ies[$i-2]=$ie;
+		$e=$e*$e%MOD;
+		$ie=$ie*$ie%MOD;
+	}
+	my$now=1;
+	my$inow=1;
+	for(0..$cnt2-2){
+		$sum_e[$_]=$es[$_]*$now%MOD;
+		$now=$now*$ies[$_]%MOD;
+		$sum_ie[$_]=$ies[$_]*$inow%MOD;
+		$inow=$inow*$es[$_]%MOD;
+	}
+}
+
+sub convolution(\@\@){
+	my@a=@{$_[0]};
+	my@b=@{$_[1]};
+	my$n=@a;
+	my$m=@b;
+	return()if$n==0||$m==0;
+	my$h=0;
+	$h++while$n+$m-1>1<<$h;
+	my$z=1<<$h;
+	my@bsf;
+	for my$i(0..$h-1){
+		for(1..1<<$h-$i-1){
+			$bsf[($_<<$i)-1]=$i;
+		}
+	}
+	#butterfly @a,@b
+	for my$ph(1..$h){
+		my$w=1<<$ph-1;
+		my$p=1<<$h-$ph;
+		my$now=1;
+		for my$s(0..$w-1){
+			my$offset=$s<<$h-$ph+1;
+			for($offset..$offset+$p-1){
+				my$l=$a[$_];
+				my$r=$a[$_+$p]*$now%MOD;
+				$a[$_]=($l+$r)%MOD;
+				$a[$_+$p]=($l-$r+MOD)%MOD;
+				$l=$b[$_];
+				$r=$b[$_+$p]*$now%MOD;
+				$b[$_]=($l+$r)%MOD;
+				$b[$_+$p]=($l-$r+MOD)%MOD;
+			}
+			$now=$now*$sum_e[$bsf[$s]]%MOD;
+		}
+	}
+	$a[$_]=$a[$_]*$b[$_]%MOD for 0..$z-1;
+	#butterfly_inv @a
+	for(my$ph=$h;$ph>=1;$ph--){
+		my$w=1<<$ph-1;
+		my$p=1<<$h-$ph;
+		my$inow=1;
+		for my$s(0..$w-1){
+			my$offset=$s<<$h-$ph+1;
+			for($offset..$offset+$p-1){
+				my$l=$a[$_];
+				my$r=$a[$_+$p];
+				$a[$_]=($l+$r)%MOD;
+				$a[$_+$p]=($l-$r+MOD)*$inow%MOD;
+			}
+			$inow=$inow*$sum_ie[$bsf[$s]]%MOD;
+		}
+	}
+	$#a=$n+$m-2;
+	my$iz=powermod($z,MOD-2);
+	$_=$_*$iz%MOD for@a;
+	return@a;
+}
+no integer;
+package main;
+my@A;
+my@B;
+for(0..<>-1){
+	($A[$_],$B[$_])=split$",<>;
+}
+my@C=Convolution::convolution(@A,@B);
+unshift@C,0;
+print$_,$/for@C;
